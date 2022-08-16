9ab6bd25781f4de5b12cdfd51d4f93ceef43a464
duytuan <ddt.tuando@gmail.com>
2022-08-17 02:09:17 +0700

    Update 404 Page

--- client/src/pages/NotFoundPage/index.tsx
+++ client/src/pages/NotFoundPage/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 
+import Header from '@/layouts/components/Header';
 import { routes } from '@/routes/routes';
 import Container from '@/components/Container';
 
 const NotFoundPage = () => {
     return (
         <>
+            <Header />
             <Container className="text-center pt-12">
                 <h1 className="font-medium text-xl">Sorry, this page isn&apos;t available.</h1>
                 <p className="mt-8 text-base">
