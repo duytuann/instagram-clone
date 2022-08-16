54e379224f5d7a289e8a1a8a24833388d6e7c6eb
duytuan <ddt.tuando@gmail.com>
2022-08-17 02:01:45 +0700

    404 Page

--- client/src/layouts/components/HeaderRightMenu/index.tsx
+++ client/src/layouts/components/HeaderRightMenu/index.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react';
 import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
+
 import { faBookmark, faUser } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRepeat, faSliders } from '@fortawesome/free-solid-svg-icons';
@@ -8,9 +10,10 @@ import { logoutStart } from '@/redux/slices/authSlice';
 
 const HeaderRightMenu = forwardRef<any, any>((_props, forwardRef) => {
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
     const {
-        data: { userToken },
+        data: { userToken, user },
     } = useAppSelector((state) => state.auth);
 
     return (
@@ -22,7 +25,12 @@ const HeaderRightMenu = forwardRef<any, any>((_props, forwardRef) => {
                 'bg-white',
             )}
         >
-            <li onClick={() => {}} className={clsx('flex items-center py-3 pl-4', 'select-none', 'hover:bg-gray-50')}>
+            <li
+                onClick={() => {
+                    navigate(`/${user.username}`);
+                }}
+                className={clsx('flex items-center py-3 pl-4', 'select-none', 'hover:bg-gray-50')}
+            >
                 <FontAwesomeIcon className="mr-3" icon={faUser} />
                 <span>Profile</span>
             </li>
---
+++ client/src/pages/404/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import { routes } from '@/routes/routes';
+import Container from '@/components/Container';
+
+const NotFount404 = () => {
+    return (
+        <>
+            <Container className="text-center pt-12">
+                <h1 className="font-medium text-xl">Sorry, this page isn&apos;t available.</h1>
+                <p className="mt-8 text-base">
+                    The link you followed may be broken, or the page may have been removed.{' '}
+                    <Link to={routes.home}>
+                        <a className="text-primary">Go back to Instagram</a>
+                    </Link>
+                    .
+                </p>
+            </Container>
+        </>
+    );
+};
+
+export default NotFount404;
