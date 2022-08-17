3a8704d2d166b3f1213923e383a622f8f2cdcf86
duytuan <ddt.tuando@gmail.com>
2022-08-17 15:43:16 +0700

    Refactor Paging DTO

---
+++ server/Instagram.API/DTO/Response/PageListDTO.cs
@@ -0,0 +1,12 @@
+namespace Instagram.API.DTO.Response;
+
+public class PageListDTO
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPrevious { get; set; }
+    public bool HasNext { get; set; }
+
+}
